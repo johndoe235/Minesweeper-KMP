d4ce4d22579b992354a85b274a951e920b7a58cf
Yosif Panchev <yosif.panchev@costacoffee.com>
2026-03-24 13:56:56 +0200

    some more small changes

--- composeApp/src/commonMain/kotlin/com/minesweeper/minesweeper/ai/MineSweeperSolver.kt
+++ composeApp/src/commonMain/kotlin/com/minesweeper/minesweeper/ai/MineSweeperSolver.kt
@@ -8,57 +8,35 @@ import com.minesweeper.minesweeper.board.deepCopy
 class MineSweeperSolver() {
 
     private val bestMovesCache: MutableList<Cell> = mutableListOf()
-    private val nodesProcessed: MutableList<Cell> = mutableListOf<Cell>()
-    private var nodesToProcess: MutableList<Cell> = mutableListOf<Cell>()
+    private val nodesProcessed: MutableList<Cell> = mutableListOf()
+    private var nodesToProcess: MutableList<Cell> = mutableListOf()
     private var foundMines = 0
 
-    private var _open: ((Int, Int) -> Char)? = null
-    private fun open(row: Int, col: Int): Char {
-        return _open!!.invoke(row, col)
-    }
-
-    fun solve(boardToSolve: Board, openSquare: (Int, Int) -> Char): Board {
-        _open = openSquare
+    fun solve(boardToSolve: Board, open: (Int, Int) -> Char): Board {
         val solvedBoard = boardToSolve.deepCopy()
-        var turns = 0
         foundMines = 0
-        var hasWonTheGame = false
         nodesToProcess = getOpenSquares(solvedBoard)
-
-
-        while (!hasWonTheGame) {
-            println(solvedBoard.format())
-            println("turns ${turns++}  mines found: $foundMines")
-
+        //1.find all mines
+        do {
             getNextMove(solvedBoard, nodesToProcess)?.let { nextSafeMove ->
                 val value = open(nextSafeMove.row, nextSafeMove.col)
                 solvedBoard[nextSafeMove.row, nextSafeMove.col] = value
 
-                if (!nodesProcessed.contains(nextSafeMove))
-                    nodesToProcess.add(nextSafeMove)
+                if (!nodesProcessed.contains(nextSafeMove)) nodesToProcess.add(nextSafeMove)
             }
+        } while (foundMines < boardToSolve.nMines)
 
-
-            if (foundMines == boardToSolve.nMines)
-                break
-
+        //open all squares that aren't flagged
+        for (row in 0 until solvedBoard.rows) {
+            for (col in 0 until solvedBoard.cols) {
+                if (solvedBoard[row, col] == MineSweeperField.HIDDEN) solvedBoard[row, col] =
+                    open(row, col)
+            }
         }
 
-        openHiddenSquares(solvedBoard)
-
-        _open = null
         return solvedBoard
     }
 
-
-    fun openHiddenSquares(solvedBoard: Board) {
-        for (row in 0 until solvedBoard.rows) {
-            for (col in 0 until solvedBoard.cols) {
-                if (solvedBoard[row, col] == MineSweeperField.HIDDEN)
-                    solvedBoard[row, col] = open(row, col)
-            }
-        }
-    }
 
     private fun getNextMove(board: Board, nodes: List<Cell>): Cell? {
         if (bestMovesCache.isNotEmpty()) {
@@ -78,15 +56,11 @@ class MineSweeperSolver() {
         sortedProbabilities.forEach {
             if (it.second > 0.999f) {
                 flag(board, it.first)
-            } else if (it.second < 0.0001f)
-                bestMovesCache.add(it.first)
+            } else if (it.second < 0.0001f) bestMovesCache.add(it.first)
         }
 
-        println(sortedProbabilities.first())
-        return if (sortedProbabilities.isEmpty() || sortedProbabilities.first().second  == 1f)
-            null
-        else
-            sortedProbabilities.first().first
+        return if (sortedProbabilities.isEmpty() || sortedProbabilities.first().second == 1f) null
+        else sortedProbabilities.first().first
     }
 
     private fun flag(board: Board, cell: Cell) {
@@ -95,24 +69,21 @@ class MineSweeperSolver() {
     }
 
 
-    private fun getOpenSquares(board: Board) =
-        mutableListOf<Cell>().also { startingPoints ->
+    private fun getOpenSquares(board: Board) = mutableListOf<Cell>().also { startingPoints ->
 
-            for (row in 0 until board.rows) {
-                for (col in 0 until board.cols) {
-                    val value = board[row, col]
-                    if (value != '?')
-                        startingPoints.add(Cell(row, col))
-                }
+        for (row in 0 until board.rows) {
+            for (col in 0 until board.cols) {
+                val value = board[row, col]
+                if (value != '?') startingPoints.add(Cell(row, col))
             }
         }
+    }
 
     private fun getRandomUnopenedCell(board: Board): Cell {
         val unOpenedCells = mutableListOf<Cell>()
         for (row in 0 until board.rows) {
             for (col in 0 until board.cols) {
-                if (board[row, col] == '?')
-                    unOpenedCells.add(Cell(row, col))
+                if (board[row, col] == '?') unOpenedCells.add(Cell(row, col))
             }
         }
         return unOpenedCells.random()
